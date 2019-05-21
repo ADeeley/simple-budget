83bb052b1817a5a81622a7525dcde5ccf12f084a
Adam Deeley <adam.deeley@oneiota.co.uk>
2019-05-21 11:05:53 +0100

    Add editable property to the list component.

--- src/app/shared/components/list/list.component.ts
+++ src/app/shared/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { TransactionService } from '../../services/transaction.service';
 import { TransactionGroup } from '../../models/transactionGroup.model';
 
@@ -11,6 +11,8 @@ export class ListComponent implements OnInit {
 
   transactionGroups: TransactionGroup;
   groupsAvailable: string[];
+  @Input() editable = false;
+
 
   constructor(private transactionService: TransactionService) { }
 
